824a8430067258dfd0395b9dc1375046932c0073
Cheklst <lvn27112002@mail.ru>
2020-09-10 20:47:10 -0700

    Добавил меню

---
+++ Project second version/Assets/MenuManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void NewGame()
+    {
+        //SceneManager.LoadScene(1);
+        Debug.Log("Рано тебе еще сюда");
+    }
+
+    public void SelectLevel()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void HowToPlay()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
