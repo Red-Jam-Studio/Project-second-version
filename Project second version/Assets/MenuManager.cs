c73423cbce9e926aba7b3f6a666d7bff033625d3
Cheklst <lvn27112002@mail.ru>
2020-09-11 20:55:45 -0700

    Добавил сцены уровней

--- Project second version/Assets/MenuManager.cs
+++ Project second version/Assets/MenuManager.cs
@@ -5,20 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    public void NewGame()
-    {
-        //SceneManager.LoadScene(1);
-        Debug.Log("Рано тебе еще сюда");
-    }
-
     public void SelectLevel()
     {
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene(1);
     }
 
     public void HowToPlay()
     {
-        SceneManager.LoadScene(3);
+        SceneManager.LoadScene(2);
     }
 
     public void Exit()
@@ -29,5 +23,55 @@ public class MenuManager : MonoBehaviour
     public void Back()
     {
         SceneManager.LoadScene(0);
+    }
+
+    public void Level1()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void Level2()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void Level3()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void Level4()
+    {
+        SceneManager.LoadScene(6);
+    }
+
+    public void Level5()
+    {
+        SceneManager.LoadScene(7);
+    }
+
+    public void Level6()
+    {
+        SceneManager.LoadScene(8);
+    }
+
+    public void Level7()
+    {
+        SceneManager.LoadScene(9);
+    }
+
+    public void Level8()
+    {
+        SceneManager.LoadScene(10);
+    }
+
+    public void Level9()
+    {
+        SceneManager.LoadScene(11);
+    }
+
+    public void Level10()
+    {
+        SceneManager.LoadScene(12);
     }
 }
