143628ef6b56d1430c314b6f2c9bab82d40e1c09
Cheklst <lvn27112002@mail.ru>
2020-09-20 20:56:46 -0700

    test battery

---
+++ Project second version/Assets/OnMouseClick.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnMouseClick : MonoBehaviour
+{
+    public GameObject Door;
+
+    void OnMouseDown()
+    {
+        Destroy(gameObject);
+
+        GameObject go = GameObject.Find("battery(Clone)");
+        if (go == null)
+        {
+            Destroy(Door);
+        }
+    }
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
