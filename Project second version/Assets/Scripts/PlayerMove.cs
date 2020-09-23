745dc3f0d5470595fd5997fbaa9c0db31ca38f3d
TorinLike <boss_potehin@list.ru>
2020-09-23 21:32:55 +0300

    Рабочее движение в функциях

--- Project second version/Assets/Scripts/PlayerMove.cs
+++ Project second version/Assets/Scripts/PlayerMove.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-enum Direction
+enum Direction // Енам (список) направлений
 {
     up,
     left,
@@ -11,50 +11,88 @@ enum Direction
 }
 public class PlayerMove : MonoBehaviour
 {
-    public double speed;
-    Direction dir = Direction.right;
+    public double moveSpeed;             // Скорость движения
+    public double rotateSpeed;           // Скорость поворота
+    Direction nextDir = Direction.right; // Следующее направление движения
+    Direction curDir = Direction.right;  // Текущее направление движения (для выбора стороны поворота с помощью сравнения с текущим)
     
 
     private void Update()
     {
-        if (Input.GetKey("w") || Input.GetKey("up"))
-        {
-            dir = Direction.up;
-        }
-        if (Input.GetKey("a") || Input.GetKey("left"))
-        {
-            dir = Direction.left;
-        }
-        if (Input.GetKey("s") || Input.GetKey("down"))
-        {
-            dir = Direction.down;
-        }
-        if (Input.GetKey("d") || Input.GetKey("right"))
-        {
-            dir = Direction.right;
-        }
+        GetNextDirection(); // Установка последней нажатой клавиши в качестве следующего направления
+        ChangeDirection();
 
         
     }
 
-    private void FixedUpdate()
+    public void GetNextDirection() // Установка последней нажатой клавиши в качестве следующего направления
     {
-        switch (dir)
+        var input = Input.inputString;
+        if (!string.IsNullOrEmpty(input))
         {
-            case Direction.up:
-                if ((transform.position.x % 2 == 0) && (transform.position.z % 2 == 0)) iTween.MoveAdd(this.gameObject, iTween.Hash("amount", new Vector3(0, 0, 2), "time", speed, "easetype", iTween.EaseType.linear));
-                break;
-            case Direction.left:
-                if ((transform.position.x % 2 == 0) && (transform.position.z % 2 == 0)) iTween.MoveAdd(this.gameObject, iTween.Hash("amount", new Vector3(-2, 0, 0), "time", speed, "easetype", iTween.EaseType.linear));
-                break;
-            case Direction.down:
-                if ((transform.position.x % 2 == 0) && (transform.position.z % 2 == 0)) iTween.MoveAdd(this.gameObject, iTween.Hash("amount", new Vector3(0, 0, -2), "time", speed, "easetype", iTween.EaseType.linear));
-                break;
-            case Direction.right:
-                if ((transform.position.x % 2 == 0) && (transform.position.z % 2 == 0)) iTween.MoveAdd(this.gameObject, iTween.Hash("amount", new Vector3(2, 0, 0), "time", speed, "easetype", iTween.EaseType.linear));
-                break;
-            default:
-                break;
+            switch (input)
+            {
+                case "w":
+                    nextDir = Direction.up;
+                    break;
+
+                case "a":
+                    nextDir = Direction.left;
+                    break;
+
+                case "s":
+                    nextDir = Direction.down;
+                    break;
+
+                case "d":
+                    nextDir = Direction.right;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+
+    public void ChangeDirection()
+    {
+        if ((transform.position.x % 2 == 0) && (transform.position.z % 2 == 0)) // Если в центре тайла
+        {
+            switch (nextDir) // Выбор направления движения в зависимости от следующего направление движения
+            {
+                case Direction.up:
+
+                    iTween.MoveAdd(this.gameObject, iTween.Hash("amount", new Vector3(0, 0, 2), "time", moveSpeed * 0.1, "easetype", iTween.EaseType.linear));
+                    if (curDir == Direction.right)
+                    {
+                        iTween.RotateAdd(this.gameObject, iTween.Hash("amount", new Vector3(0, -90, 0), "time", rotateSpeed, "easetype", iTween.EaseType.linear));
+                    }
+                    //iTween.RotateAdd(this.gameObject, iTween.Hash("amount", new Vector3(0, 0, 2), "time", rotateSpeed, "easetype", iTween.EaseType.linear));
+                    //curDir = Direction.up;
+
+                    break;
+
+                case Direction.left:
+
+                    iTween.MoveAdd(this.gameObject, iTween.Hash("amount", new Vector3(-2, 0, 0), "time", moveSpeed * 0.1, "easetype", iTween.EaseType.linear));
+
+                    break;
+
+                case Direction.down:
+
+                    iTween.MoveAdd(this.gameObject, iTween.Hash("amount", new Vector3(0, 0, -2), "time", moveSpeed * 0.1, "easetype", iTween.EaseType.linear));
+
+                    break;
+
+                case Direction.right:
+
+                    iTween.MoveAdd(this.gameObject, iTween.Hash("amount", new Vector3(2, 0, 0), "time", moveSpeed * 0.1, "easetype", iTween.EaseType.linear));
+
+                    break;
+
+                default:
+                    break;
+            }
         }
     }
 }
