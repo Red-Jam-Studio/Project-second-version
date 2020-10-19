3e6c64c6f7ba68a6477861983c56955cafd67841
Kate Name <Katusha_krr@whatever.com>
2020-10-19 14:26:55 +0300

    Pool test

---
+++ Project second version/Assets/Scripts/CubeSpawn.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class CubeSpawn : MonoBehaviour
+{
+    public GameObject PoolCube;
+
+    void FixedUpdate()
+    {
+        Instantiate(PoolCube, transform.position, Quaternion.identity);
+    }
+}
